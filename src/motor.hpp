5c486ed82d0a559c45ecaea286f5f64dac89a246
HIAndris <144882697+HIAndris@users.noreply.github.com>
2025-06-16 10:07:50 +0200

    Update motor.hpp

--- src/motor.hpp
+++ src/motor.hpp
@@ -9,13 +9,12 @@ typedef uint32_t                motor_delay_t; // delay between motor steps in m
 typedef uint32_t                motor_total_t; // cycles needed to complete a 360 degree turn (a cycle is a rotation of bits on the motor pins)
 typedef float                   motor_accel_t; // acceleration rate of the motor (0 < a < 1) (0.1 = rapid acceleration, 0.9 = slow acceleration)
 
-volatile int8_t Controller::speed = 0;
-int64_t Controller::motor_state = 0;
-
 
 
 class DriveMotor {
   public:
+    volatile int8_t speed;
+    
     void init(
         const motor_pins_t& pins,
         const motor_delay_t min_start_delay,
@@ -26,6 +25,8 @@ class DriveMotor {
     void go(uint16_t cycles, motor_delay_t delay);
 
   private:
+    int64_t motor_state;
+  
     motor_pins_t  pins;
     motor_delay_t start;
     motor_delay_t top;
